f1fdb37ee7561d6851d2c2ca81124bc01b70ca58
mmm <aaa123@pobox.sk>
2022-06-21 18:07:56 +0200

    fixed inventory to work with items with zero count

--- src/main/java/com/github/industrialcraft/inventorysystem/Inventory.java
+++ src/main/java/com/github/industrialcraft/inventorysystem/Inventory.java
@@ -15,10 +15,17 @@ public class Inventory {
         this.data = data;
     }
     public ItemStack getAt(int index){
-        return this.items[index];
+        ItemStack is = this.items[index];
+        if(is != null && is.getCount() <= 0){
+            items[index] = null;
+        }
+        return items[index];
     }
     public void setAt(int index, ItemStack itemStack){
-        this.items[index] = itemStack;
+        if(itemStack != null && itemStack.getCount() <= 0)
+            this.items[index] = null;
+        else
+            this.items[index] = itemStack;
     }
     public void putAt(int index, ItemStack is, EItemPutMode mode){
         putAt(index, is, mode.isOverwrite(), mode.isDump());
@@ -29,14 +36,14 @@ public class Inventory {
             item = null;
         if(overwrite) {
             if (canPut(index, item)) {
-                if(items[index] != null && dump)
+                if(getAt(index) != null && dump)
                     overflow(items[index]);
 
                 this.items[index] = item;
             }
         } else {
             if (canPut(index, item)) {
-                if (items[index] == null) {
+                if (getAt(index) == null) {
                     items[index] = is;
                     return;
                 }
@@ -71,7 +78,7 @@ public class Inventory {
     public void addItem(ItemStack item){
         int toAdd = item.getCount();
         for(int i = 0;i < items.length;i++){
-            if(items[i]!=null&&item.stacks(items[i]) && canPut(i, item)){
+            if(getAt(i)!=null&&item.stacks(items[i]) && canPut(i, item)){
                 int toSupply = item.getItem().getStackSize()-items[i].getCount();
                 if(toSupply > 0) {
                     toAdd -= toSupply;
@@ -82,7 +89,7 @@ public class Inventory {
             }
         }
         for(int i = 0;i < items.length;i++){
-            if(items[i] == null && canPut(i, item)){
+            if(getAt(i) == null && canPut(i, item)){
                 items[i] = item.clone(toAdd);
                 return;
             }
@@ -97,6 +104,8 @@ public class Inventory {
             return false;
         int toRemove = count;
         for(int i = 0;i < items.length;i++){
+            if(getAt(i)==null)
+                continue;
             if(p.test(items[i]))
                 continue;
             int removed = Math.min(toRemove,items[i].getCount());
@@ -126,25 +135,27 @@ public class Inventory {
     }
     public void dropAll(){
         for(ItemStack is : items){
-            if(is != null)
+            if(is != null && is.getCount() > 0)
                 overflow(is);
         }
         clear();
     }
     public void drop(int slot){
-        ItemStack is = items[slot];
+        ItemStack is = getAt(slot);
+        if(is == null)
+            return;
         overflow(is);
         items[slot] = null;
     }
     public boolean isEmpty(){
         for(ItemStack is : items){
-            if(is != null)
+            if(is != null && is.getCount() > 0)
                 return false;
         }
         return true;
     }
-    public Object getData() {
-        return data;
+    public <T> T getData() {
+        return (T) data;
     }
     public int getSize() {
         return items.length;
